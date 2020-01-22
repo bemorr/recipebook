53c74e6900f399b2a5e012af2bb565c588124e9f
bemorr <benmorrisldnuk@gmail.com>
2020-01-22 18:11:18 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    cons = require('consolidate'),
+    dust = require('dustjs-helpers'),
+    pg = require('pg')
+    app = express();
+
+// DB connect string
+var connect= "postgres://testTest:9QPI^Kdb6@localhost/recipebookDB";
+
+// Assign Dust Engine to .dust files
+app.engine('dust', cons.dust)  
+
+// set default Ext .dust
+app.set('view engine', 'dust');
+app.set('views',__dirname + '/views');
+
+// set public folder
+app.set(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false})) // permits and parses nested objects when set to true ONLY strings and arrays when false.
+
+app.get('/', function(req, res){
+    console.log('Test')
+});
+
+app.listen(3000, function(){
+    console.log('Server started on port 3000')
+});
