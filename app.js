925596d375de80be7eb1c54a8d58c0673a47f7b4
bemorr <benmorrisldnuk@gmail.com>
2020-01-24 15:50:57 +0000

    up to implementing the DB connection function

--- app.js
+++ app.js
@@ -3,30 +3,46 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     cons = require('consolidate'),
     dust = require('dustjs-helpers'),
-    pg = require('pg')
+    pg = require('pg'),
     app = express();
 
 // DB connect string
+const { Pool, Client } = require('pg');
 var connect= "postgres://testTest:9QPI^Kdb6@localhost/recipebookDB";
 
 // Assign Dust Engine to .dust files
-app.engine('dust', cons.dust)  
+app.engine('dust', cons.dust);
 
 // set default Ext .dust
 app.set('view engine', 'dust');
 app.set('views',__dirname + '/views');
 
 // set public folder
-app.set(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false})) // permits and parses nested objects when set to true ONLY strings and arrays when false.
+app.use(bodyParser.urlencoded({ extended: false})); // permits and parses nested objects when set to true ONLY strings and arrays when false.
 
 app.get('/', function(req, res){
-    console.log('Test')
+    const pool = new Pool({
+        connect: connect,
+      })
+      pool.query('SELECT * FROM recipes', (err, res) => {
+        console.log(err, res)
+        pool.end()
+      })
+      const client = new Client({
+        connect: connect,
+      })
+      client.connect()
+      client.query('SELECT * FROM recipes', (err, res) => {
+        console.log(err, res)
+        res.render('index', {recipes: res.rows})
+        client.end()
+      })
 });
 
 app.listen(3000, function(){
-    console.log('Server started on port 3000')
+    console.log('Server started on port 3000');
 });
