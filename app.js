2983a7bbf607b820337e16111b03f211fc10d3bd
bemorr <benmorrisldnuk@gmail.com>
2020-01-27 13:31:57 +0000

    recipes(name/ingredient/dircetions) pulling from PGDB and initial buttons added for display,edit delete

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express = require('express'),
 
 // DB connect string
 const { Pool, Client } = require('pg');
-var connect= "postgres://testTest:9QPI^Kdb6@localhost/recipebookDB";
+var connect= "postgres://testTest:9QPI^Kdb6@localhost:5432/recipebookDB";
 
 // Assign Dust Engine to .dust files
 app.engine('dust', cons.dust);
@@ -25,24 +25,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false})); // permits and parses nested objects when set to true ONLY strings and arrays when false.
 
 app.get('/', function(req, res){
-    const pool = new Pool({
+    // PG connect
+    const client = new Client ({
         connect: connect,
-      })
-      pool.query('SELECT * FROM recipes', (err, res) => {
-        console.log(err, res)
-        pool.end()
-      })
-      const client = new Client({
-        connect: connect,
-      })
-      client.connect()
-      client.query('SELECT * FROM recipes', (err, res) => {
-        console.log(err, res)
-        res.render('index', {recipes: res.rows})
-        client.end()
-      })
+        })
+    client.connect() 
+    client.query('SELECT * FROM recipes', (err, res1) => {
+        console.log(err, res1);
+        res.render('index', {recipes: res1.rows});
+        client.end();
+    });
 });
 
 app.listen(3000, function(){
     console.log('Server started on port 3000');
-});
+})
