64881e7cf0c28e2b6f9ca62e3dff5353880cdd82
bemorr <benmorrisldnuk@gmail.com>
2020-01-29 21:58:18 +0100

    edit functionality added, dotenv pkg installed + .env gitignore

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     cons = require('consolidate'),
     dust = require('dustjs-helpers'),
-    pg = require('pg'),
+    { Client } = require('pg'),
+    dotenv = require('dotenv').config(),
     app = express();
-
-// DB connect string
-const { Pool, Client } = require('pg');
-var connect= "postgres://testTest:9QPI^Kdb6@localhost:5432/recipebookDB";
 
 // Assign Dust Engine to .dust files
 app.engine('dust', cons.dust);
@@ -27,16 +24,17 @@ app.use(bodyParser.urlencoded({ extended: false})); // permits and parses nested
 app.get('/', function(req, res){
     // PG connect
     const client = new Client ({
-        connectionString: connect,
-        host: 'localhost',
-        port: 5432,
-        user: 'testTest',
-        password: '9QPI^Kdb6',
-        database: 'recipebookDB'
+        // dotenv pkg managing creds 
+        connectionString: process.env.CONNECTIONSTRING,
+        host: process.env.HOST,
+        port: process.env.PORT,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
         })
     client.connect() 
     client.query('SELECT * FROM recipes', (err, res1) => {
-        console.log(err); //, res1);
+        console.log(err);
         res.render('index', {recipes: res1.rows});
         client.end();
     });
@@ -46,19 +44,20 @@ app.post('/add', function(req, res){
     const text = "INSERT INTO recipes(name, ingredients, directions) VALUES($1, $2, $3)";
     const values = [req.body.name, req.body.ingredients, req.body.directions];
     const client = new Client ({
-        connectionString: connect,
-        host: 'localhost',
-        port: 5432,
-        user: 'testTest',
-        password: '9QPI^Kdb6',
-        database: 'recipebookDB'
+        // dotenv pkg managing creds.
+        connectionString: process.env.CONNECTIONSTRING,
+        host: process.env.HOST,
+        port: process.env.PORT,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
         })
 
     client.connect();
     client
         .query(text, values)
         .then(result => console.log(result))
-        .catch(e => console.error(e.stack, result))
+        .catch(e => console.error(e.stack))
         .then(() => client.end());
     res.redirect("/");
 })
@@ -67,22 +66,46 @@ app.delete("/delete/:id", function (req,res) {
     const text = "DELETE FROM recipes WHERE id = $1";
     const values = [req.params.id];
     const client = new Client ({
-        connectionString: connect,
-        host: 'localhost',
-        port: 5432,
-        user: 'testTest',
-        password: '9QPI^Kdb6',
-        database: 'recipebookDB'
+        // dotenv pkg managing creds.
+        connectionString: process.env.CONNECTIONSTRING,
+        host: process.env.HOST,
+        port: process.env.PORT,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
         })
 
     client.connect();
     client
         .query(text, values)
         .then(result => console.log(result))
-        .catch(e => console.error(e.stack, result))
+        .catch(e => console.error(e.stack))
         .then(() => client.end());
     res.send(200);
-})
+});
+
+
+app.post('/edit', function(req, res){
+    const text = "UPDATE recipes SET name=$1, ingredients=$2, directions=$3 WHERE id=$4";
+    const values = [req.body.name, req.body.ingredients, req.body.directions, req.body.id];
+    const client = new Client ({
+        // dotenv pkg managing creds.
+        connectionString: process.env.CONNECTIONSTRING,
+        host: process.env.HOST,
+        port: process.env.PORT,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
+        })
+    client.connect();
+    client
+        .query(text, values)
+        .then(result => console.log(result))
+        .catch(e => console.error(e.stack))
+        .then(() => client.end());
+    res.redirect('/');
+});
+
 
 app.listen(3000, function(){
     console.log('Server started on port 3000');
